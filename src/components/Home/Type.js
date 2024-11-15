56e1fdd89dc797c8127ed9f29c1a163c5312baa7
SandaAbhishekSagar <144849115+SandaAbhishekSagar@users.noreply.github.com>
2024-11-15 11:23:11 -0500

    committing latest changes with resumes

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -10,7 +10,7 @@ function Type() {
           "Software Developer",
           "Full Stack Developer",
           "MERN Stack Developer",
-          "Open Source Contributor",
+          
         ],
         autoStart: true,
         loop: true,
