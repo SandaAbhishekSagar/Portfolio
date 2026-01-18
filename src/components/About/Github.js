36696d2392f5655aa281a4a1b826cb280a272881
Abhishek Sagar Sanda <144849115+SandaAbhishekSagar@users.noreply.github.com>
2026-01-18 14:23:53 -0500

    github fix

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -61,21 +61,30 @@ function Github() {
             contributions in the last year
           </p>
         )}
-        <div style={{ display: "flex", justifyContent: "center", width: "100%", paddingTop: "10px" }}>
+        <div 
+          style={{ 
+            display: "flex", 
+            justifyContent: "center", 
+            width: "100%", 
+            paddingTop: "10px",
+            paddingBottom: "20px",
+            overflowX: "auto"
+          }}
+          className="github-calendar-wrapper"
+        >
           <GitHubCalendar
             username="SandaAbhishekSagar"
             blockSize={15}
             blockMargin={5}
-            fontSize={16}
+            fontSize={14}
+            hideTotalCount={false}
+            hideColorLegend={false}
             theme={{
               level0: "#161b22",
               level1: "#0e4429",
               level2: "#006d32",
               level3: "#26a641",
               level4: "#39d353",
-            }}
-            style={{
-              color: "#8b949e",
             }}
           />
         </div>
